6bfd04954b005e9e4212cd59ddfe242b2e451f0e
djarae <djaraestivill@gmail.com>
2024-05-12 04:18:25 -0400

    Productos avance

--- Documentacion.md
+++ Documentacion.md
@@ -1,3 +1,7 @@
-# Siguiente a realizar: Vue CLI crear componente
+# Siguiente a realizar: Vue CLI crear componente 10.4.24
 https://cli.vuejs.org/#getting-started
 https://reintech.io/blog/tutorial-using-vue-js-cli-create-components
+
+# Regla de Vue: Debe escribirse en "Multi-word"(camel case,snake case ,etc. 12.5.24)
+https://eslint.vuejs.org/rules/multi-word-component-names
+https://www.linkedin.com/pulse/naming-multi-word-variables-programming-languages-inelu-hiscc/
---
+++ src/components/HomeApp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div >
+    <p>
+       Nueva wea
+    </p>
+    <ProductosApp msg="Welcome to Your Vue.js App"/>
+
+  </div>
+</template>
+
+<script>
+import ProductosApp from './ProductosApp/ProductosApp.vue'
+
+export default {
+  name: 'HomeApp',
+  props: {
+    msg: String
+  },
+  components: {
+    ProductosApp
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
